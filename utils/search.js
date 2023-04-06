3ede3e91ab27b37695ac22dd1c5567da53955b93
mushfiqueyeasir <mushfiqueyeasir@gmail.com>
2023-04-06 09:30:39 +0600

    query  changed

--- utils/search.js
+++ utils/search.js
@@ -2,10 +2,12 @@ module.exports.search = (query, data) => {
   const { search, startDate, endDate, page, email } = query;
   let paginationResult = data;
   if (email) {
-    paginationResult = data.filter((item) => item.eventCreator === email);
+    paginationResult = paginationResult.filter(
+      (item) => item.eventCreator === email
+    );
   }
   if (search) {
-    paginationResult = data.filter(
+    paginationResult = paginationResult.filter(
       (item) =>
         item.eventTitle.toLowerCase().includes(search.toLowerCase()) ||
         item.eventLocation.toLowerCase().includes(search.toLowerCase())
@@ -13,7 +15,7 @@ module.exports.search = (query, data) => {
   }
 
   if (page) {
-    paginationResult = data.slice(
+    paginationResult = paginationResult.slice(
       ((page ? page : 1) - 1) * 10,
       (page ? page : 1) * 10
     );
