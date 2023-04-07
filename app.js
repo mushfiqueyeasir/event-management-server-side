7d9a966788938746f04f1104de0b64f7d4284329
mushfiqueyeasir <mushfiqueyeasir@gmail.com>
2023-04-07 13:49:36 +0600

    body-parser   added

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { errorHandler } = require("./middleware/errorHandeller");
+const bodyParser = require("body-parser");
 
 // thard party middleWare
 app.use(cors());
 app.use(express.json());
 app.use(errorHandler);
+
+// Increase the maximum request size to 10MB
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
 // route
 const eventsRoutes = require("./routes/v1/events.routes");
